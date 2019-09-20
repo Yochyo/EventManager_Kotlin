63778bc0627c5b801ff519479b7826493e61fcfa
Philipp <herodragon25@gmail.com>
2019-09-20 14:40:42 +0200

    retainAll, ... in EventCollection are now useable

--- src/main/java/de/yochyo/eventmanager/EventCollection.kt
+++ src/main/java/de/yochyo/eventmanager/EventCollection.kt
@@ -4,26 +4,26 @@ import java.util.function.Predicate
 
 
 open class EventCollection<T>(private val c: MutableCollection<T>) : MutableCollection<T> {
-    val onUpdate = object: EventHandler<OnUpdateEvent>(){}
-    val onClear = object: EventHandler<OnClearEvent>(){
+    val onUpdate = object : EventHandler<OnUpdateEvent>() {}
+    val onClear = object : EventHandler<OnClearEvent>() {
         override fun trigger(e: EventCollection<T>.OnClearEvent) {
             super.trigger(e)
             notifyChange()
         }
     }
-    val onAddElement = object: EventHandler<OnAddElementEvent>(){
+    val onAddElement = object : EventHandler<OnAddElementEvent>() {
         override fun trigger(e: EventCollection<T>.OnAddElementEvent) {
             super.trigger(e)
             notifyChange()
         }
     }
-    val onAddElements = object: EventHandler<OnAddElementsEvent>(){
+    val onAddElements = object : EventHandler<OnAddElementsEvent>() {
         override fun trigger(e: EventCollection<T>.OnAddElementsEvent) {
             super.trigger(e)
             notifyChange()
         }
     }
-    val onRemoveElement = object: EventHandler<OnRemoveElementEvent>(){
+    val onRemoveElement = object : EventHandler<OnRemoveElementEvent>() {
         override fun trigger(e: EventCollection<T>.OnRemoveElementEvent) {
             super.trigger(e)
             notifyChange()
@@ -34,7 +34,7 @@ open class EventCollection<T>(private val c: MutableCollection<T>) : MutableColl
         notifyChange()
     }
 
-    fun notifyChange(){
+    fun notifyChange() {
         onUpdate.trigger(OnUpdateEvent(c))
     }
 
@@ -71,24 +71,38 @@ open class EventCollection<T>(private val c: MutableCollection<T>) : MutableColl
     }
 
     override fun removeAll(elements: Collection<T>): Boolean {
-        val res = c.removeAll(elements)
-        if (res)
-            notifyChange()
-        return res
+        var removed = false
+        for (e in elements)
+            if (remove(e)) removed = true
+        return removed
     }
 
     override fun removeIf(filter: Predicate<in T>): Boolean {
-        val res = c.removeIf(filter)
-        if (res)
-            notifyChange()
-        return res
+        var removed = false
+        val iter = c.iterator()
+        while (iter.hasNext()) {
+            val current = iter.next()
+            if (filter.test(current)) {
+                iter.remove()
+                removed = true
+                onRemoveElement.trigger(OnRemoveElementEvent(c, current))
+            }
+        }
+        return removed
     }
 
     override fun retainAll(elements: Collection<T>): Boolean {
-        val res = c.retainAll(elements)
-        if (res)
-            notifyChange()
-        return res
+        val iter = c.iterator()
+        var removed = false
+        while(iter.hasNext()){
+            val current = iter.next()
+            if(!elements.contains(current)){
+                iter.remove()
+                onRemoveElement.trigger(OnRemoveElementEvent(c, current))
+                removed = true
+            }
+        }
+        return removed
     }
 
     override fun iterator() = c.iterator()
@@ -96,9 +110,9 @@ open class EventCollection<T>(private val c: MutableCollection<T>) : MutableColl
 
     override fun spliterator() = c.spliterator()
 
-    inner class OnUpdateEvent(val collection: Collection<T>): Event()
-    inner class OnClearEvent(val collection: Collection<T>): Event()
-    inner class OnAddElementEvent(val collection: Collection<T>,val  element: T): Event()
-    inner class OnAddElementsEvent(val collection: Collection<T>,val  elements: Collection<T>): Event()
-    inner class OnRemoveElementEvent(val collection: Collection<T>,val  element: T): Event()
+    inner class OnUpdateEvent(val collection: Collection<T>) : Event()
+    inner class OnClearEvent(val collection: Collection<T>) : Event()
+    inner class OnAddElementEvent(val collection: Collection<T>, val element: T) : Event()
+    inner class OnAddElementsEvent(val collection: Collection<T>, val elements: Collection<T>) : Event()
+    inner class OnRemoveElementEvent(val collection: Collection<T>, val element: T) : Event()
 }
