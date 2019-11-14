a1f447a924ffccb0f4d94472e87e4f942c0ef1a1
Philipp <herodragon25@gmail.com>
2019-11-14 16:44:07 +0100

    move EventHandler to own File

--- src/main/java/de/yochyo/eventmanager/Event.kt
+++ src/main/java/de/yochyo/eventmanager/Event.kt
@@ -2,43 +2,8 @@ package de.yochyo.eventmanager
 
 import java.util.*
 
-open class EventHandler<E: Event>{
-    private val lock = Any()
-    private val listeners = LinkedList<Listener<E>>()
-    fun registerListener(l: Listener<E>){
-        synchronized(lock){
-            val res = listeners.add(l)
-            listeners.sortBy { it }
-        }
-    }
-    fun registerListener(priority: Int = Listener.NORMAL, l: (e: E)->Unit): Listener<E>{
-        val listener = object : Listener<E>() {
-            override fun onEvent(e: E) = l(e)
-        }
-        registerListener(listener)
-        return listener
-    }
-
-    fun removeListener(l: Listener<E>) = synchronized(lock){listeners.remove(l)}
-    fun removeAllListeners() = synchronized(lock){listeners.clear()}
-
-    open fun trigger(e: E) {
-        synchronized(lock){
-            val iter = listeners.iterator()
-            while(iter.hasNext() && !e.isCanceled){
-                iter.next().onEvent(e)
-                if(e.deleteListener){
-                    e.deleteListener = false
-                    iter.remove()
-                }
-            }
-        }
-    }
-}
-
 abstract class Event {
     open val name: String
         get() = this::class.java.simpleName!!
-    var isCanceled = false
     var deleteListener = false
 }
