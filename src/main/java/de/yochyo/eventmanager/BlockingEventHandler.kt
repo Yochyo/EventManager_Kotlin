5f1ead661348e58b6c4b197dcfeea07723058760
Philipp <herodragon25@gmail.com>
2019-11-14 16:54:21 +0100

    Add SuspendedEventHandler/Listener for apps using kotlinx.coroutines

--- src/main/java/de/yochyo/eventmanager/BlockingEventHandler.kt
+++
@@ -1,9 +0,0 @@
-package de.yochyo.eventmanager
-
-open class BlockingEventHandler<E : Event> : EventHandler<E>() {
-    protected val lock = Any()
-    override fun registerListener(l: Listener<E>) = synchronized(lock) { super.registerListener(l) }
-    override fun removeAllListeners() = synchronized(lock) { super.removeAllListeners() }
-    override fun removeListener(l: Listener<E>) = synchronized(lock) { super.removeListener(l) }
-    override fun trigger(e: E) = synchronized(lock) { super.trigger(e) }
-}
---
+++ src/main/java/de/yochyo/eventmanager/NonBlockingEventHandler.kt
@@ -0,0 +1,33 @@
+package de.yochyo.eventmanager
+
+import java.util.*
+
+open class NonBlockingEventHandler<E : Event> {
+    private val listeners = LinkedList<Listener<E>>()
+    open fun registerListener(l: Listener<E>) {
+        listeners.add(l)
+        listeners.sortBy { it }
+    }
+
+    fun registerListener(priority: Int = Listener.NORMAL, l: (e: E) -> Unit): Listener<E> {
+        val listener = object : Listener<E>() {
+            override fun onEvent(e: E) = l(e)
+        }
+        registerListener(listener)
+        return listener
+    }
+
+    open fun removeListener(l: Listener<E>) = listeners.remove(l)
+    open fun removeAllListeners() = listeners.clear()
+
+    open fun trigger(e: E) {
+        val iter = listeners.iterator()
+        while (iter.hasNext()) {
+            iter.next().onEvent(e)
+            if (e.deleteListener) {
+                e.deleteListener = false
+                iter.remove()
+            }
+        }
+    }
+}
