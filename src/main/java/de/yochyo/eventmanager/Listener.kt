004ee79712a402257e373c7f2862c3e3f396c3f8
Yochyo <herodragon25@gmail.com>
2020-09-11 14:54:40 +0200

    removed unneeded Listener.create method

--- src/main/java/de/yochyo/eventmanager/Listener.kt
+++ src/main/java/de/yochyo/eventmanager/Listener.kt
@@ -1,12 +1,5 @@
 package de.yochyo.eventmanager
 
 fun interface Listener<T : Event> {
-    companion object {
-        fun <E : Event> create(l: (e: E) -> Unit): Listener<E> {
-            val listener = Listener<E> { l(it) }
-            return listener
-        }
-    }
-
     fun onEvent(e: T)
 }
